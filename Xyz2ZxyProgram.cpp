1c54490bf21583bf26cbcdd21a82145918392cfd
Takashi Michikawa <michikawa@acm.org>
2022-05-16 15:14:44 +0900

    make_sample16.cpp added for 16bit image validation, and confirmed the new codes can handle  uncompressed 16bit images.

--- Xyz2ZxyProgram.cpp
+++ Xyz2ZxyProgram.cpp
@@ -29,7 +29,6 @@
 #include <mi/thread_safe_counter.hpp>
 #include <mi/repeat.hpp>
 #include <mi/progress_bar.hpp>
-
 Xyz2ZxyProgram::Xyz2ZxyProgram(const mi::Argument &arg) : mi::ProgramTemplate(arg, "xyz2zxy", fmt::format("v.{}", XYZ2ZXY_VERSION)), output_dir_("output"), num_(100) , extension_(".tif"){
         std::filesystem::path input_dir;
         this->getAttributeSet().createAttribute("-i", input_dir).setMessage("Input directory").setMandatory();
@@ -41,8 +40,9 @@ Xyz2ZxyProgram::Xyz2ZxyProgram(const mi::Argument &arg) : mi::ProgramTemplate(ar
                 this->getAttributeSet().printUsage();
                 throw std::runtime_error("Insufficient arguments");
         }
-        std::remove_copy_if (std::filesystem::directory_iterator(input_dir), std::filesystem::directory_iterator(), std::back_inserter(this->image_paths_),[&ext = this->extension_](const auto& f){
-                return std::filesystem::is_directory(f) || f.path().filename().string().find_first_of(".") == 0 || f.path().extension()!= ext;});
+        std::copy_if (std::filesystem::directory_iterator(input_dir), std::filesystem::directory_iterator(), std::back_inserter(this->image_paths_),[&ext = this->extension_](const auto& f){
+                return !std::filesystem::is_directory(f) && f.path().filename().string().find_first_of(".") != 0 && f.path().extension()== ext;}
+                );
         if (this->image_paths_.empty()){
                 throw std::runtime_error("Empty images");
         }
@@ -55,6 +55,17 @@ Xyz2ZxyProgram::~Xyz2ZxyProgram() {
 
 bool
 Xyz2ZxyProgram::run() {
+        auto write_image = [](const std::string filename, const cv::Mat& image) {
+                if (image.depth() <= 2 ) {
+                        std::vector<int> params = {cv::IMWRITE_TIFF_COMPRESSION, 1};
+                        cv::imwrite(filename, image, params);
+                } else {
+                        std::cerr<<"Unsupported depth:"<<image.depth()<<std::endl;
+                        return false;
+                }
+                return true;
+        };
+        
         auto& paths = this->image_paths_;
         std::filesystem::path tmpDir = this->output_dir_.string() + "_temp";
         std::filesystem::create_directories(tmpDir);
@@ -74,7 +85,7 @@ Xyz2ZxyProgram::run() {
         const uint32_t step = uint32_t(this->num_);
 
         // get volume size
-        cv::Mat image = cv::imread(paths[0].string(), cv::IMREAD_ANYDEPTH | cv::IMREAD_ANYCOLOR);
+        cv::Mat image = cv::imread(paths[0].string(), cv::IMREAD_UNCHANGED);
         const uint32_t sx = uint32_t(image.size().width);
         const uint32_t sy = uint32_t(image.size().height);
         image.release();
@@ -86,15 +97,15 @@ Xyz2ZxyProgram::run() {
         for (uint32_t z = 0; z < sz; z += step) {
                 std::vector<cv::Mat> images;
                 const uint32_t end = (z + step < sz) ? z + step : sz;
-                std::transform(paths.begin() + z, paths.begin() + end, std::back_inserter(images),[](auto& f){return cv::imread(f.string(), cv::IMREAD_ANYDEPTH | cv::IMREAD_ANYCOLOR);});
+                std::transform(paths.begin() + z, paths.begin() + end, std::back_inserter(images),[](auto& f){return cv::imread(f.string(), cv::IMREAD_UNCHANGED);});
                 std::filesystem::create_directory(tmpDir/std::to_string(z));
-                mi::repeat_mt([&counter, &images, &sx, &sy, &z, &get_tmp_filename]() {
+                mi::repeat_mt([&counter, &images, &sx, &sy, &z, &get_tmp_filename, &write_image]() {
                         for (uint32_t y = counter.get(); y < sy; y = counter.get()) {
                                 std::vector<cv::Mat> local_images;
                                 std::transform(images.begin(), images.end(), std::back_inserter(local_images), [&y, &sx](auto &image) {return cv::Mat(image, cv::Rect(cv::Point(0, int(y)), cv::Size(int(sx), 1)));}); // cut
                                 cv::Mat local;
                                 cv::vconcat(local_images, local);
-                                cv::imwrite(get_tmp_filename(y, z), local);
+                                write_image(get_tmp_filename(y, z), local);
                         }
                 });
                 mi::progress_bar(z + uint32_t(images.size()), sz, step1Str);
@@ -105,7 +116,7 @@ Xyz2ZxyProgram::run() {
         mi::progress_bar<uint32_t>(num_of_finished.get(), sy, "Step2 concat");
         std::mutex mtx;
         mi::repeat_mt(
-                [&num_of_finished, &mtx, &counter, &get_tmp_filename, &outputdir = this->output_dir_, &sy, &step, &sz]() {
+                [&num_of_finished, &mtx, &counter, &get_tmp_filename, &outputdir = this->output_dir_, &sy, &step, &sz, &write_image]() {
                         for (uint32_t y = counter.get(); y < sy; y = counter.get()) {
                                 std::vector<cv::Mat> local_images;
                                 for (uint32_t  z = 0; z < sz; z += step) {
@@ -115,7 +126,7 @@ Xyz2ZxyProgram::run() {
                                 cv::Mat result;
                                 cv::vconcat(local_images, result);
                                 cv::flip(result, result, 0); // mirroring
-                                cv::imwrite(outputdir.string()+"/"+fmt::format("image-{:05d}.tif", y), result);
+                                write_image(outputdir.string()+"/"+fmt::format("image-{:05d}.tif", y), result);
                                 mi::progress_bar(mtx, num_of_finished.get(), sy, "Step2 concat");
                         }
                 });
@@ -123,3 +134,6 @@ Xyz2ZxyProgram::run() {
         std::filesystem::remove_all(tmpDir);
         return true;
 }
+
+
+
